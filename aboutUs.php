b030b13ae004934cab7379b0fd590ad169ba343a
thananstr <thanan.sstwp@gmail.com>
2016-11-09 23:21:53 +0700

    not completed

---
+++ aboutUs.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+    <title>MU-Tram Project</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="search.css" />
+    <link rel="stylesheet" type="text/css" href="profileAboutUs.css">
+  </head>
+  <body>
+    <div id="menu">
+      <ul class="menubar">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="location.php">Location</a></li>
+        <li><a href="aboutUs.php">About us</a></li>
+        <li style="float: right"><a href="#" >Language</a></li>
+        <li style="float: right"><a href="#" >Search<div id="search">
+        </div></a></li>
+      </ul>
+    <div id="page">
+      <table>
+        <tr>
+          <td><h1>Member</h1></td>
+        </tr>
+        <tr>
+          <td>
+            <img class="image-border" src="image\ProfilePic_Pond.jpg">
+            <img class="image-borderWithSpace" src="image\ProfilePic_Ice.jpg">
+            <img class="image-borderWithSpace" src="image\ProfilePic_Max.jpg">
+            <img class="image-borderWithSpace" src="image\ProfilePic_Art.jpg">
+          </td>
+        </tr>
+        <tr>
+          <td style="height:50px">
+            <label class="profileDetail">Name1</label>
+            <label class="profileDetailWithSpace">Name2</label>
+            <label class="profileDetailWithSpace">Name3</label>
+            <label class="profileDetailWithSpace">Name4</label>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div id="credit">Faculty of ICT, Mahidol University</div>
+  </body>
+</html>
