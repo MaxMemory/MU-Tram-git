1f9a571d0ee974def93b410e697edb4f31d3b392
MaxMemory <maxsomonkey@gmail.com>
2016-11-21 20:43:23 +0700

    update eiei 555

--- load-information-user.php
+++ load-information-user.php
@@ -12,13 +12,44 @@
     $id = $_POST['album'];
     $id = substr($id,13);
 
-    $sql = 'SELECT stationID, stationName, image FROM stations WHERE stationID = '.$id.';';
+    $sql = 'SELECT stationID, stationName, image, green, blue, red FROM stations WHERE stationID = '.$id.';';
     $data = $connect->query($sql);
+
+
     $row = $data->fetch_assoc();
     $image = $row["image"];
     $station = $row["stationName"];
     $_SESSION["stationID"] = $id;
     $_SESSION["banner"] = $row["stationName"];
+    $tram_green = "";
+    $tram_blue = "";
+    $tram_red = "";
+
+
+
+    if($row["green"] == 1){
+      $sql = 'SELECT timeGeneral, timeSpecial FROM schedule WHERE stationID = '.$id.' and color = "green";';
+      $timeSql = $connect->query($sql);
+      $timeGreen = $timeSql->fetch_assoc();
+      $tram_green = "True";
+      // $hour = date("h");
+      // $min = date("i");
+    }
+
+    if($row["blue"] == 1){
+      $sql = 'SELECT timeGeneral, timeSpecial FROM schedule WHERE stationID = '.$id.' and color = "blue";';
+      $timeSql = $connect->query($sql);
+      $timeblue = $timeSql->fetch_assoc();
+      $tram_blue = "True";
+    }
+
+    if($row["red"] == 1){
+      $sql = 'SELECT timeGeneral, timeSpecial FROM schedule WHERE stationID = '.$id.' and color = "red";';
+      $timeSql = $connect->query($sql);
+      $timeRed = $timeSql->fetch_assoc();
+      $tram_red = "True";
+    }
+
 ?>
 <html>
 <head>
@@ -30,48 +61,87 @@
 
 </head>
 <body>
-  <h1><?php echo $station; ?></h1>
-  <form action="check-upload-photo.php" method="post" enctype="multipart/form-data">
-  <table style="width: 100%; margin: 10px">
-    <tr>
-      <td>
-          <?php
-          echo '<img id="location-image" src="'.$image.'">';
-          ?>
-      </td>
-    </tr>
-    <tr>
-        <td>
-          <p>other Photos</p>
-        </td>
-    </tr>
-    <tr>
-      <td style="margin: 20px">
+  <div style="padding: 10px">
+    <h1><?php echo $station; ?></h1>
+    <form action="check-upload-photo.php" method="post" enctype="multipart/form-data">
+    <div class="row">
+      <div class="col-xs-12 center">
+        <?php
+        echo '<img id="location-image" src="'.$image.'">';
+        ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+          <h3>Schedule</h3>
+      </div>
+    </div>
+    <?php
+      if($tram_green == "True"){
+    ?>
+      <div class="row">
+        <div class="col-xs-1">
+          <canvas class="object_Green"></canvas>
+        </div>
+        <div class="col-xs-11">
+          <a style="color: Green">Green: </a>
+          20 min left
+        </div>
+      </div>
+    <?php
+      }
+      if($tram_blue == "True"){
+    ?>
+    <div class="row">
+      <div class="col-xs-1">
+        <canvas class="object_Blue"></canvas>
+      </div>
+      <div class="col-xs-11">
+        <a style="color: Blue" >Blue: </a>
+        20 min left
+      </div>
+    </div>
+    <?php
+      }
+      if($tram_red == "True"){
+    ?>
+    <div class="row">
+      <div class="col-xs-1">
+        <canvas class="object_Red"></canvas>
+      </div>
+      <div class="col-xs-11">
+        <a style="color: Red">Red: </a>
+        20 min left
+      </div>
+    </div>
+    <?php } ?>
+    <hr style="margin: 10px"/>
+    <div class="row">
+      <div class="col-xs-12">
+            <h3>Other Photos</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
         <?php
         echo '<img class="other-photos" src="'.$image.'">';
         echo '<img class="other-photos" src="'.$image.'">';
         echo '<img class="other-photos" src="'.$image.'">';
         ?>
-      </td>
-    </tr>
-    <?php
-      if(isset($_SESSION['login-user'])){
-        ?><tr>
-            <td style="margin: 20px">
-              <label style="margin-top:10px;color:white;background-color:28B5C7" for="files" class="btn">Add photos</label>
-              <input id="files" style="visibility:hidden;" name="updateProfile" type="file">
-              <button style="margin-left:10px" type="submit">upload</button>
-            </td>
-        </tr><?php
-      }
-
-
-
-
-
-
-    ?>
-  </table>
-  </form>
+      </div>
+    </div>
+      <?php
+        if(isset($_SESSION['login-user'])){
+          ?>
+          <div class="row" style="margin-top: 10px">
+            <div class="col-xs-12">
+                <label style="color:white;background-color:28B5C7" for="files" class="btn">Add photos</label>
+                <input id="files" style="visibility:hidden;" name="updateProfile" type="file">
+                <button type="submit">upload</button>
+            </div>
+          </div>
+        <?php } ?>
+    </form>
+  </div>
 </body>
 </html>
